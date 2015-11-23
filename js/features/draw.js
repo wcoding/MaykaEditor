ef1a0e0e50db6816106c1ba80e1c5c462bf23584
wcoding <mailbox@wcoding.ru>
2015-11-23 17:02:28 +0700

    minicolors init

--- js/features/draw.js
+++ js/features/draw.js
@@ -6,5 +6,27 @@ $(document).ready(function () {
 		var str = '#data-content #sidebar' + ' .' + clas;
 		var content = $(str).html();
 		$('.sidebar #var').replaceWith('<div id="var">' + content + '</div>');
+
+		// Minicolors init */
+	    $('.demo').each( function() {
+	        $(this).minicolors({
+	            control: $(this).attr('data-control') || 'hue',
+	            defaultValue: $(this).attr('data-defaultValue') || '',
+	            format: $(this).attr('data-format') || 'hex',
+	            keywords: $(this).attr('data-keywords') || '',
+	            inline: $(this).attr('data-inline') === 'true',
+	            letterCase: $(this).attr('data-letterCase') || 'lowercase',
+	            opacity: $(this).attr('data-opacity'),
+	            position: $(this).attr('data-position') || 'bottom left',
+	            change: function(value, opacity) {
+	                if( !value ) return;
+	                if( opacity ) value += ', ' + opacity;
+	                if( typeof console === 'object' ) {
+	                    console.log(value);
+	                }
+	            },
+	            theme: 'bootstrap'
+	        });
+	    });
 	});
 });
